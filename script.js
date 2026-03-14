1f3a901b67ee7077e9e571b8b61260d53846f2d8
yassir cherqui <yassirch245@gmail.com>
2026-03-15 00:54:07 +0100

    ajouter validation de form

--- script.js
+++ script.js
@@ -10,9 +10,12 @@ const errurtele = document.getElementById("errurtele");
 const errurmatch = document.getElementById("errurmatch");
 const errurticket = document.getElementById("errurticket");
 const success = document.getElementById("success");
-
+let id = 0;
+let total = 0;
+let reservations = [];
 form.addEventListener('submit',function(e){
     e.preventDefault();
+   id++;
 let nametest = /^[A-Za-z\s]{3,}$/;
 let emailtest = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/;
 if(!nametest.test(nameinp.value)){
@@ -42,4 +45,50 @@ else{
 }
 
 
+let reservation = {
+    id:id,
+    nom:nameinp.value,
+    email:email.value,
+    telephone:telephone.value,
+    match:match.value,
+    nbTickets:nticket.value,
+    prix:100,
+    prixTotal : 100*nticket.value
+}
+reservations.push(reservation);
+
+// console.log(reservations);
+afficherreservation();
+
+
 })
+
+    let totalprix =0;
+
+
+function afficherreservation(){
+    list.innerHTML = "";
+    if(reservations.length ==0){
+      list.innerHTML =`<tr><td>aucune reservation</td></tr>`;
+      return;
+    }
+    reservations.forEach(function(res){
+        list.innerHTML = `<tr>
+            <td>${res.match}</td>
+            <td>${res.nbTickets}</td>
+            <td>${res.prix}</td>
+            <td>${res.prixTotal}</td>
+            <td><button class="btn btn-danger btn-sm" onclick="supprimerReservation(${res.id})">Supprimer</button></td>
+            </tr>`;
+            totalprix = totalprix + res.prixTotal;
+    });
+    
+}
+
+let totalpr = document.getElementById('total');
+totalpr.textContent = "price total de reservation est"+totalprix;
+
+
+
+
+
